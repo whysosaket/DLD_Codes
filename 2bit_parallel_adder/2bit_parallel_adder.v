b7e6345709da33fe5c61c19424c94445124d76ee
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-12-10 23:26:12 +0530

    Update 2bit_parallel_adder.v

--- 2bit_parallel_adder/2bit_parallel_adder.v
+++ 2bit_parallel_adder/2bit_parallel_adder.v
@@ -6,5 +6,5 @@ module bit_parallel_adder(a0,a1,b0,b1,s0,s1,co);
   assign w1 = a0&b0;  // carry out for a0b0 half_adder
 
   assign s1 = a1^b1^w1; // s1 digit
-  assign co = !(!(a1&b1))|(!(b1&w1))|(!(a1&w1)); // 
+  assign co = !((!(a1&b1))|(!(b1&w1))|(!(a1&w1))); // 
 endmodule
